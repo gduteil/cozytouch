02c739d9d7f2202b008df512fd17e12e2ad05eeb
Olivier B <hobbe@users.noreply.github.com>
2025-02-19 23:06:05 +0100

    Add support for Sauter Asama Connecté II Ventilo 1750W Blanc

--- custom_components/cozytouch/model.py
+++ custom_components/cozytouch/model.py
@@ -248,6 +248,14 @@ def get_model_infos(modelId: int, zoneName: str | None = None):
             4: HVACMode.HEAT,
         }
 
+    elif modelId == 1543:
+        modelInfos["name"] = "Asama Connecté II Ventilo 1750W Blanc"
+        modelInfos["type"] = CozytouchDeviceType.TOWEL_RACK
+        modelInfos["HVACModes"] = {
+            0: HVACMode.OFF,
+            4: HVACMode.HEAT,
+        }
+
     else:
         modelInfos["name"] = "Unknown product (" + str(modelId) + ")"
         modelInfos["type"] = CozytouchDeviceType.UNKNOWN
